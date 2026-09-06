7c8148c2f23492be37dec1d53e8f010958d1d8a0
Tejas <tejaskumar29pk@gmail.com>
2026-09-06 22:33:18 +0530

    Time: 0 ms (100%), Space: 19.4 MB (19.92%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,25 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write = 0
+        read = 0
+    
+        while read < len(chars):
+            char = chars[read]
+            count = 0
+        
+        # Count consecutive identical characters
+            while read < len(chars) and chars[read] == char:
+                read += 1
+                count += 1
+            
+        # Write the character
+            chars[write] = char
+            write += 1
+        
+        # If the count is greater than 1, write its digits
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+                
+        return write
